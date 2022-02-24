a463547ad3882c9bd73bbb2c684d688cde05f0b5
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2022-02-24 14:07:19 -0300

    finish tests

--- routes/routers.test.js
+++ routes/routers.test.js
@@ -1,14 +1,15 @@
 const request = require("supertest");
 const { app } = require("../index");
 const mongoose = require("mongoose");
+const generateRandomEmail = require("../helpers/generateRandomEmail");
 const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
 jest.mock("../middlewares/authenticatorMiddleware", () => (req, res, next) => next());
 
 describe("Test the routers path", () => {
-  const testUserInfo = {
-    _id: "61f3088edde97b91ee1cbcd5",
-    email: "kluserhuu@gmail.com",
-    password: "1234",
+  const newUserInfo = {
+    name: "Steve Jobs",
+    email: generateRandomEmail(10),
+    password: "4321",
   }
 
   beforeAll((done) => {
@@ -21,6 +22,25 @@ describe("Test the routers path", () => {
       mongoose.disconnect(done);
   });
 
+  test("It should response '/user/insert-user' router", async () => {
+    expect.assertions(4);
+
+    // arrange
+    const successCode = 201;
+    const failureCode = 400;
+
+    // act
+    const responseSuccess = await request(app).post("/user/insert-user").send(newUserInfo);
+    const responseFailure = await request(app).post("/user/insert-user").send({});
+    newUserInfo._id = responseSuccess.body.data._id;
+
+    // assert
+    expect(responseSuccess.statusCode).toBe(successCode);
+    expect(responseSuccess.body.data.email).toBe(newUserInfo.email);
+    expect(responseSuccess.body.data.password).toBe(newUserInfo.password);
+    expect(responseFailure.statusCode).toBe(failureCode);
+  });
+
   test("It should response '/login' router", async () => {
     expect.assertions(4);
 
@@ -29,13 +49,13 @@ describe("Test the routers path", () => {
     const failureCode = 400;
 
     // act
-    const responseSuccess = await request(app).post("/login").send(testUserInfo);
+    const responseSuccess = await request(app).post("/login").send(newUserInfo);
     const responseFailure = await request(app).post("/login").send({});
     
     // assert
     expect(responseSuccess.statusCode).toBe(successCode);
-    expect(responseSuccess.body.data.email).toBe(testUserInfo.email);
-    expect(responseSuccess.body.data.password).toBe(testUserInfo.password);
+    expect(responseSuccess.body.data.email).toBe(newUserInfo.email);
+    expect(responseSuccess.body.data.password).toBe(newUserInfo.password);
     expect(responseFailure.statusCode).toBe(failureCode);
   });
 
@@ -47,13 +67,29 @@ describe("Test the routers path", () => {
     const failureCode = 400;
 
     // act
-    const responseSuccess = await request(app).get(`/user/${testUserInfo._id}`);
+    const responseSuccess = await request(app).get(`/user/${newUserInfo._id}`);
     const responseFailure = await request(app).get(`/user/1337`);
 
     // assert
     expect(responseSuccess.statusCode).toBe(successCode);
-    expect(responseSuccess.body.data.email).toBe(testUserInfo.email);
-    expect(responseSuccess.body.data.password).toBe(testUserInfo.password);
+    expect(responseSuccess.body.data.email).toBe(newUserInfo.email);
+    expect(responseSuccess.body.data.password).toBe(newUserInfo.password);
+    expect(responseFailure.statusCode).toBe(failureCode);
+  })
+
+  test("It should response '/user/delete-by-body' router", async () => {
+    expect.assertions(2);
+
+    // arrange
+    const successCode = 200;
+    const failureCode = 400;
+
+    // act
+    const responseSuccess = await request(app).delete("/user/delete-by-body").send(newUserInfo);
+    const responseFailure = await request(app).delete("/user/delete-by-body").send({ _id: "1337" });
+
+    // assert
+    expect(responseSuccess.statusCode).toBe(successCode);
     expect(responseFailure.statusCode).toBe(failureCode);
   })
 });
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,7 +3,7 @@ const userController = require('../controllers/userController');
 const authentificate = require('../middlewares/authenticatorMiddleware');
 const router = express.Router({ mergeParams: true });
 
-router.post('/insert-user', authentificate, userController.insertUser);
+router.post('/insert-user', userController.insertUser);
 router.get('/get-all-users', authentificate, userController.getAllUsers);
 router.get('/get-all-users-email', authentificate, userController.getAllUsersEmail);
 router.get('/get-by-query', authentificate, userController.getByQuery);
@@ -11,5 +11,6 @@ router.get('/:id', authentificate, userController.getUserById);
 router.put('/update', authentificate, userController.updateUser);
 router.put('/replace', authentificate, userController.replaceUser);
 router.delete('/delete', authentificate, userController.deleteUser);
+router.delete('/delete-by-body', authentificate, userController.deleteByBody);
 
 module.exports = router;
